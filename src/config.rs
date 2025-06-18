da3284235cfb8204220ea1e37b6dbdbb7cf3036e
Kate the Gay <kanako@gayest.dev>
2025-06-17 21:17:15 -0700

    Add basic config struct

---
+++ src/config.rs
@@ -0,0 +1,24 @@
+pub struct Location {
+    pub longitude: f64,
+    pub latitude: f64,
+    pub name: Option<String>
+}
+
+pub struct Config {
+    pub timezone: String,
+    pub time_format: String,
+    pub collection: String,
+    pub location: Option<Location>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            timezone: String::from("Etc/UTC"),
+            time_format: String::from("%-I:%-M"),
+            // https://unsplash.com/collections/1053828/tabliss-official
+            collection: String::from("1053828"),
+            location: None
+        }
+    }
+}
